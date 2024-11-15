d385540c41f0ffbae1bb9fbdfe65dd9a7bbb09a8
Luis David Rodriguez Arias <luis.1701721583@ucaldas.edu.co>
2024-11-15 14:16:01 -0500

    fix: names db

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask, request, jsonify
 from chain.prompt_handler import procesar_mensaje
-from audio.audio import GrabadoraDeVoz
+import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+# Endpoint para obtener el JSON de los clientes
+@app.route('/api/clientes', methods=['GET'])
+def obtener_clientes():
+    with open('data/db.json', 'r', encoding='utf-8') as file:
+        clientes = json.load(file)
+    return jsonify(clientes)
 
 @app.route('/api/cobranza', methods=['POST'])
 def cobrar():
@@ -11,23 +20,5 @@ def cobrar():
     return jsonify({"respuesta": respuesta})
     
 
-def main():
-    # Instanciamos la clase GrabadoraDeVoz
-    grabadora = GrabadoraDeVoz()  # Puedes pasar otros parámetros si lo necesitas, como el umbral de silencio o la duración
-
-    # Grabar audio
-    archivo_wav = grabadora.grabar_audio()
-
-    # Reconocer el texto del audio
-    texto = grabadora.reconocer_voz()
-
-    if texto:
-        # Imprimir el texto reconocido
-        print("Texto reconocido:", texto)
-
-    # Convertir y guardar el archivo en formato MP3
-    archivo_mp3 = grabadora.convertir_a_mp3()
-
 if __name__ == "__main__":
     app.run(debug=True)
-    main()
